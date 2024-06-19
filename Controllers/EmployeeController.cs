9c263bbfc160cabe29179616b90986d2276d3e98
Dhiulia Antunes <antunesdhiulia@gmail.com>
2024-06-19 20:19:10 -0300

    find by id fix

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -82,7 +82,7 @@ namespace Clinic.Controllers
             if (user == null)
                 return NotFound();
             
-            return Ok();
+            return Ok(user);
         }
     }
 }
